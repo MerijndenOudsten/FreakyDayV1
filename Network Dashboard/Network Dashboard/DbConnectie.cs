e3bc0cb86f114fa82be0eaf10123f43e481c053c
MerijndenOudsten <m.denoudsten@student.fontys.nl>
2016-10-11 13:19:47 +0200

    Gebruieker, DbConnectie, DbQueries

---
+++ Network Dashboard/Network Dashboard/DbConnectie.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Network_Dashboard
+{
+    public class DbConnectie
+    {
+        private static string connString = "Data Source=192.168.15.50/fhictora;port=1521;User Id=dbi319035;Password=deathispeace;";
+
+
+        public static DataTable SelecteerData(OracleCommand command)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+
+                using (OracleConnection connection = new OracleConnection())
+                {
+                    connection.ConnectionString = connString;
+                    connection.Open();
+
+                    command.Connection = connection;
+
+                    OracleDataReader reader = command.ExecuteReader();
+                    dataTable.Load(reader);
+
+                    return dataTable;
+                }
+            }
+            catch (OracleException e)
+            {
+                Console.Write(e.Message);
+                return null;
+            }
+        }
+
+        public static bool CommandUitvoeren(OracleCommand command)
+        {
+            try
+            {
+                using (OracleConnection connection = new OracleConnection())
+                {
+                    connection.ConnectionString = connString;
+                    connection.Open();
+
+                    command.Connection = connection;
+
+                    int check = command.ExecuteNonQuery();
+
+                    if (check > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (OracleException e)
+            {
+                Console.Write(e.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ Network Dashboard/Network Dashboard/Gebruiker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_Dashboard
+{
+    public class Gebruiker
+    {
+        public int Id { get; set; }
+        public string Inlognaam { get; set; }
+        public string Wachtwoord { get; set; }
+
+        public Gebruiker(string inlognaam, string wachtwoord)
+        {
+            this.Inlognaam = inlognaam;
+            this.Wachtwoord = wachtwoord;
+        }
+    }
+}
